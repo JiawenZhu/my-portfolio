2d535171a3ee9ba71b464643dfe4e4583a374aad
Jiawen Zhu <jzhu10@mail.sfsu.edu>
2018-03-20 17:40:45 -0700

    update the html to ejs

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.get('/', function (req, res){
+	res.render('index');
+	// res.send('<h1> This is jiawen home page </h1>');
+});
+
+app.get('/about', function(req,res){
+	res.render('about');
+});
+app.listen(3000, function(){
+	console.log("listen to port 3000");
+});
