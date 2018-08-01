81bd0564f916c07a170c9cda554d83a887b23864
Jiawen Zhu <jzhu10@mail.sfsu.edu>
2018-07-31 19:27:50 -0700

    update

--- app.js
+++ app.js
@@ -22,24 +22,3 @@ app.get('/footer', function(req,res){
 app.listen(process.env.PORT || 3000, function(){
 	console.log("listen to port 3000");
 });
-var nodemailer = require('nodemailer');
-var transporter = nodemailer.createTransport({
-	service: 'gmail',
-	auth:{
-		user:'zhujiawen519@gmail.com',
-		pass:'Jw*8997290'
-	}
-});
-var mailOptions = {
-	from: 'zhujiawen519@gmail.com',
-	to: 'jzhu10@mail.sfsu.edu',
-	subject: 'sending a message',
-	text: 'This is a text message'
-};
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
-  }
-});
